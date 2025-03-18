45f40784453f8b5ae80edb2c502b3c3d10270a83
Cemus <24947582+Cemus@users.noreply.github.com>
2025-03-18 13:47:55 +0100

    Get all products

--- src/main/java/com/kev/springrest/controller/product/ProductController.java
+++ src/main/java/com/kev/springrest/controller/product/ProductController.java
@@ -23,6 +23,7 @@ public class ProductController {
     @GetMapping("/products")
     public String findAllProducts(Model model) {
         Iterable<Product> products = productService.findAll();
+        System.out.println(products);
         if (products.iterator().hasNext()) {
             model.addAttribute("products", products);
         }
@@ -32,7 +33,6 @@ public class ProductController {
     @GetMapping("/product/{id}")
     public String findProductById(Model model, @Valid @PathVariable long id) {
         Optional<Product> product = productService.findById(id);
-        String error = "";
         System.out.println(product);
         product.ifPresent(value -> model.addAttribute("product", value));
 
